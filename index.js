5855ee51a73ac03518f1ae5326f07293a389eee3
Christian Hiltscher <hiltscher.christian@gmail.com>
2016-07-08 20:48:36 +0200

    update, added decapitalizing

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 var helpBy = {};
 
-helpBy.capitalize = function(string){
-    if(!(typeof(string)=='string'))
+helpBy.capitalizing = function(string) {
+    if (!(typeof(string) == 'string'))
         return -1;
-    return string[0].toUpperCase()+string.slice(1);
+    return string[0].toUpperCase() + string.slice(1);
+}
+
+helpBy.decapitalizing = function(string) {
+    if (!(typeof(string) == 'string'))
+        return -1;
+    return string[0].toLowerCase() + string.slice(1);
 }
 
 module.exports = helpBy;
