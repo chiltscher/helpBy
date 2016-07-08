e921c74ca45fa3a9f1de0f74b55d998c33f677b1
Christian Hiltscher <hiltscher.christian@gmail.com>
2016-07-08 23:27:21 +0200

    added removing function to remove an element from an array

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const util = require('util');
+
 var helpBy = {};
 
 helpBy.capitalizing = function(string) {
@@ -10,6 +12,21 @@ helpBy.decapitalizing = function(string) {
     if (!(typeof(string) == 'string'))
         return -1;
     return string[0].toLowerCase() + string.slice(1);
+}
+
+helpBy.removing = function(array, el) {
+
+    if(!util.isArray(array))
+        return -1
+
+    var index = array.indexOf(el);
+
+    if(index==-1)
+        return index;
+
+    array.splice(index, 1);
+
+    return array;
 }
 
 module.exports = helpBy;
